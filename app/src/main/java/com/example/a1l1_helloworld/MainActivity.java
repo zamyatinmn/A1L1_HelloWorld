b9e66acdf8793d8cc4263799bcb7aa1d3850e4ff
zamyatinmn <zamyatinmn@gmail.com>
2020-08-02 15:44:01 +0300

    lesson3

--- app/src/main/java/com/example/a1l1_helloworld/MainActivity.java
+++ app/src/main/java/com/example/a1l1_helloworld/MainActivity.java
@@ -1,14 +1,18 @@
 package com.example.a1l1_helloworld;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
     private TextView textView;
     private final double COEFFICIENT = 1.5;
     private Button settingsBtn;
+    private final String windValueDataKey = "windValueDataKey";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,16 +29,75 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         initViews();
         setOnClickBehavior();
+        Toast.makeText(this, "onCreate", Toast.LENGTH_SHORT).show();
+        Log.d("TAG", "onCreate");
+        setOnSwitchBehavior();
+    }
+
+    private void setOnSwitchBehavior() {
+        wtgSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    textView.setText(String.valueOf((int) ((double) windValue * COEFFICIENT)));
+                } else {
+                    textView.setText(String.valueOf(windValue));
+                }
+
+            }
+        });
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle SaveInstanceState) {
+        String value = textView.getText().toString();
+        SaveInstanceState.putString(windValueDataKey, value);
+        super.onSaveInstanceState(SaveInstanceState);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        String value = savedInstanceState.getString(windValueDataKey);
+        textView.setText(value);
+    }
+
+    @Override
+    protected void onStart() {
+        Toast.makeText(this, "onStart", Toast.LENGTH_SHORT).show();
+        Log.d("TAG", "onStart");
+        super.onStart();
+    }
+
+    @Override
+    protected void onPause() {
+        Toast.makeText(this, "onPause", Toast.LENGTH_SHORT).show();
+        Log.d("TAG", "onPause");
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        Toast.makeText(this, "onResume", Toast.LENGTH_SHORT).show();
+        Log.d("TAG", "onResume");
+        super.onResume();
+    }
+
+    @Override
+    protected void onStop() {
+        Toast.makeText(this, "onStop", Toast.LENGTH_SHORT).show();
+        Log.d("TAG", "onStop");
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        Toast.makeText(this, "onDestroy", Toast.LENGTH_SHORT).show();
+        Log.d("TAG", "onDestroy");
+        super.onDestroy();
     }
 
     private void setOnClickBehavior() {
-        wtgSwitch.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                windValue = (int) ((double) windValue * COEFFICIENT);
-                textView.setText(String.valueOf(windValue));
-            }
-        });
         settingsBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
